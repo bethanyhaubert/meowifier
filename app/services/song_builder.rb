0554105f5bddbc0d90aa4f43ce1876c6d8ad5f69
Beth Haubert <bethany.haubert@gmail.com>
2016-11-24 17:23:00 -0600

    refactored song_builder

--- app/services/song_builder.rb
+++ app/services/song_builder.rb
@@ -2,20 +2,21 @@ require 'wavefile'
 include WaveFile
 
 class SongBuilder
-	def initialize(collection, new_file_name)
+	OUTPUT_FORMAT = Format.new(:stereo, :pcm_16, 44100)
+	SAMPLE_FRAMES_PER_BUFFER = 4096
+
+	def initialize(collection, file_name=Time.now.to_i)
 		@collection = collection
-		@new_file_name = new_file_name
+		@file_name = file_name
+		#TODO: Store this file on S3
+		@file_path = "#{Rails.root}/app/assets/songs/#{@file_name}.wav"
+		combine
 	end
 
 	def combine
-		@file_name = Time.now.to_i
-		sample_frames = 4096
-		format = Format.new(:stereo, :pcm_16, 44100)
-		File.new("#{Rails.root}/app/assets/songs/#{@file_name}.wav", "w")
-
-		Writer.new("#{Rails.root}/app/assets/songs/#{@file_name}.wav", format) do |writer|
+		Writer.new(@file_path, OUTPUT_FORMAT) do |writer|
 			@collection.each do |file|
-				Reader.new(file).each_buffer(sample_frames) do |buffer|
+				Reader.new(file).each_buffer(SAMPLE_FRAMES_PER_BUFFER) do |buffer|
 					writer.write(buffer)
 				end
 			end
@@ -24,19 +25,24 @@ class SongBuilder
 
 	def combined_duration_of_collection
 		duration = 0
-		index = -1
 		@collection.each do |file|
-			reader = Reader.new(file)
-			milliseconds = (reader.total_duration.seconds * 1000) + reader.total_duration.milliseconds
-			duration += milliseconds
-			index += 1
+			duration += duration_of_individual_file(file)
 		end
-		duration + index
+		duration + duration_adjustment
 	end
 
 	def duration_of_song
-		combine
-		reader = Reader.new("#{Rails.root}/app/assets/songs/#{@file_name}.wav")
+		duration_of_individual_file(@file_path)
+	end
+
+private
+
+	def duration_of_individual_file(file)
+		reader = Reader.new(file)
 		(reader.total_duration.seconds * 1000) + reader.total_duration.milliseconds
+	end
+
+	def duration_adjustment
+		@collection.length-1
 	end
 end
--- spec/services/song_builder_spec.rb
+++ spec/services/song_builder_spec.rb
@@ -4,21 +4,19 @@ RSpec.describe SongBuilder do
   it "combines two audio files" do
 		file_1 = "#{Rails.root}/app/assets/meows/C4.wav"
     file_2 = "#{Rails.root}/app/assets/meows/D4.wav"
-		file_name = "Jingle Bells"
 		collection = [file_1, file_2]
-		song_builder = SongBuilder.new(collection, file_name)
+		song_builder = SongBuilder.new(collection)
 
-		expect(song_builder.combined_duration_of_collection).to eq(song_builder.duration_of_song)
+		expect(song_builder.duration_of_song).to eq(5851)
 	end
 
   it "combines three audio files" do
     file_1 = "#{Rails.root}/app/assets/meows/C4.wav"
     file_2 = "#{Rails.root}/app/assets/meows/D4.wav"
     file_3 = "#{Rails.root}/app/assets/meows/E4.wav"
-    file_name = "Jingle Bells"
     collection = [file_1, file_2, file_3]
-    song_builder = SongBuilder.new(collection, file_name)
+    song_builder = SongBuilder.new(collection)
 
-    expect(song_builder.combined_duration_of_collection).to eq(song_builder.duration_of_song)
+    expect(song_builder.duration_of_song).to eq(8777)
 	end
 end
