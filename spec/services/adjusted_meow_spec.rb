a8fb49ee63323e936b05d1155591f1f365024505
Sumeet Jain <sumeet@sumeetjain.com>
2016-11-24 14:24:35 -0600

    Some starter tests

---
+++ spec/services/adjusted_meow_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe AdjustedMeow do
+  pending "creates a 0.25 second meow from a 0.80 second meow"
+  pending "creates a 0.60 second meow from a 0.40 second meow"
+end
---
+++ spec/services/song_builder_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe SongBuilder do
+  pending "combines two audio files"
+  pending "combines three audio files"
+end
