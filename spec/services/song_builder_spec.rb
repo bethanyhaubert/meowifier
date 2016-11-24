10d85f220cdc3b6bf47a62bad2053e75972d87c3
Sumeet Jain <sumeet@sumeetjain.com>
2016-11-24 14:29:20 -0600

    Added a note about the test

--- spec/services/song_builder_spec.rb
+++ spec/services/song_builder_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 RSpec.describe SongBuilder do
   pending "combines two audio files"
   pending "combines three audio files"
+
+  # How to verify:
+  # 
+  # If the file was combined correctly, the duration of the file should equal
+  # the combined durations of the multiple files that comprise it.
+  # 
+  # See the "Getting Metadata About a Wave File" section at
+  # http://wavefilegem.com/examples.html for help on getting that info!
 end
