9e91067cd97e77f3ff6edbafee40afe69ee87387
Beth Haubert <bethany.haubert@gmail.com>
2016-11-26 11:41:43 -0600

    added note converter and specs

---
+++ spec/services/note_converter_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe NoteConverter do
+  it "converts white key" do
+    midi_num = 24
+		note_converter = NoteConverter.new(midi_num)
+
+		expect(note_converter.note).to eq("c1")
+	end
+
+	it "converts black key" do
+    midi_num = 22
+    note_converter = NoteConverter.new(midi_num)
+
+    expect(note_converter.note).to eq("a0#")
+	end
+
+  it "converts intermediate number to nearest note" do
+    midi_num = 62.587276458740234375
+    note_converter = NoteConverter.new(midi_num)
+
+    expect(note_converter.note).to eq("d4#")
+  end
+end
