0d967e1c644b53be705659c68805916157053bea
Beth Haubert <bethany.haubert@gmail.com>
2016-11-27 13:22:54 -0600

    adjusted meow changes

--- app/services/adjusted_meow.rb
+++ app/services/adjusted_meow.rb
@@ -1,3 +1,19 @@
+require 'wavefile'
+include WaveFile
+
 class AdjustedMeow
 
+	def initialize(meow, duration)
+		@meow = meow
+		@new_duration = duration
+	end
+
+	def adjust
+
+	end
+
+	def duration_of_meow_in_seconds(meow)
+		reader = Reader.new(meow)
+		(reader.total_duration.minutes * 60000).to_f + reader.total_duration.seconds.to_f + (reader.total_duration.milliseconds.to_f/1000.0)
+	end
 end
--- spec/services/adjusted_meow_spec.rb
+++ spec/services/adjusted_meow_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AdjustedMeow do
-  pending "creates a 0.25 second meow from a 0.80 second meow"
-  pending "creates a 0.60 second meow from a 0.40 second meow"
+  it "creates a 0.25 second meow from a 0.741 second meow" do
+    meow = "#{Rails.root}/app/assets/meows/c4.wav"
+    duration = 0.25
+		adjust = AdjustedMeow.new(meow, duration)
+		# adjusted_meow = adjust.adjust
+
+		expect(adjust.duration_of_meow_in_seconds(meow)).to eq(0.741)
+		# exepct(adjust.duration_of_meow_in_seconds(adjusted_meow)).to eq(0.25)
+	end
+
+  it "creates a 1.25 second meow from a 0.762 second meow" do
+    meow = "#{Rails.root}/app/assets/meows/c-4.wav"
+    duration = 1.25
+    adjust = AdjustedMeow.new(meow, duration)
+
+    expect(adjust.duration_of_meow_in_seconds(meow)).to eq(0.762)
+	end
 end
