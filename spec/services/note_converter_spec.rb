49df9918a3975f3fbd2dade88838febecdf75a57
Beth Haubert <bethany.haubert@gmail.com>
2016-11-26 18:10:42 -0600

    added octave adjusters in note converter

--- spec/services/note_converter_spec.rb
+++ spec/services/note_converter_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe NoteConverter do
   it "converts white key" do
-    midi_num = 24
+    midi_num = 60
 		note_converter = NoteConverter.new(midi_num)
 
-		expect(note_converter.note).to eq("c1")
+		expect(note_converter.note).to eq("c4")
 	end
 
 	it "converts black key" do
-    midi_num = 22
+    midi_num = 61
     note_converter = NoteConverter.new(midi_num)
 
-    expect(note_converter.note).to eq("a#0")
+    expect(note_converter.note).to eq("c#4")
 	end
 
   it "converts intermediate number to nearest note" do
@@ -20,5 +20,33 @@ RSpec.describe NoteConverter do
     note_converter = NoteConverter.new(midi_num)
 
     expect(note_converter.note).to eq("d#4")
-  end
+	end
+
+	it "raises the octave of a note by 3" do
+		midi_num = 22
+		note_converter = NoteConverter.new(midi_num)
+
+		expect(note_converter.note).to eq ("a#3")
+	end
+
+	it "raises the octave of a note by 2" do
+		midi_num = 34
+		note_converter = NoteConverter.new(midi_num)
+
+		expect(note_converter.note).to eq ("a#3")
+	end
+
+	it "raises the octave of a note by 1" do
+		midi_num = 46
+		note_converter = NoteConverter.new(midi_num)
+
+		expect(note_converter.note).to eq ("a#3")
+	end
+
+	it "lowers the octave of a note by 1" do
+		midi_num = 102
+		note_converter = NoteConverter.new(midi_num)
+
+		expect(note_converter.note).to eq ("f#6")
+	end
 end
