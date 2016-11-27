c78533ade682c2717ba5668e8ddab96511a3432d
Beth Haubert <bethany.haubert@gmail.com>
2016-11-26 20:49:26 -0600

    added lower octave

--- spec/services/song_builder_spec.rb
+++ spec/services/song_builder_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe SongBuilder do
   it "combines two audio files" do
-		file_1 = "#{Rails.root}/app/assets/meows/C4.wav"
-    file_2 = "#{Rails.root}/app/assets/meows/D4.wav"
+		file_1 = "#{Rails.root}/app/assets/meows/beth_C4.wav"
+    file_2 = "#{Rails.root}/app/assets/meows/beth_D4.wav"
 		collection = [file_1, file_2]
 		song_builder = SongBuilder.new(collection)
 
@@ -11,21 +11,21 @@ RSpec.describe SongBuilder do
 	end
 
   it "combines three audio files" do
-    file_1 = "#{Rails.root}/app/assets/meows/C4.wav"
-    file_2 = "#{Rails.root}/app/assets/meows/D4.wav"
-    file_3 = "#{Rails.root}/app/assets/meows/E4.wav"
+    file_1 = "#{Rails.root}/app/assets/meows/beth_C4.wav"
+    file_2 = "#{Rails.root}/app/assets/meows/beth_D4.wav"
+    file_3 = "#{Rails.root}/app/assets/meows/beth_E4.wav"
     collection = [file_1, file_2, file_3]
     song_builder = SongBuilder.new(collection)
 
     expect(song_builder.duration_of_song).to eq(8777)
 	end
 
-	it "creates the Game of Thrones theme" do
-		song = "https://s3.amazonaws.com/meowifier-staging/test_songs/got.mp3"
-		song_parser = SongParser.new(song)
-		parsed_song = song_parser.notes
-		song_builder = SongBuilder.new(parsed_song)
-
-		expect(song_builder)
-	end
+	# it "creates the Game of Thrones theme" do
+	# 	song = "https://s3.amazonaws.com/meowifier-staging/test_songs/got.mp3"
+	# 	song_parser = SongParser.new(song)
+	# 	parsed_song = song_parser.notes
+	# 	song_builder = SongBuilder.new(parsed_song)
+	#
+	# 	expect(song_builder)
+	# end
 end
