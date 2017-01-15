50e50be22241d6907ff68a7826b0966743f2c406
Beth Haubert <bethany.haubert@gmail.com>
2017-01-15 16:49:33 -0600

    FFMPEG working

--- app/services/meow_file_matcher.rb
+++ app/services/meow_file_matcher.rb
@@ -12,15 +12,19 @@ class MeowFileMatcher
 		index = 0
 		@formatted_notes.each do |note|
 			output_file = "#{Rails.root}/app/assets/outputs/#{note["note"]}-#{Time.now}-#{index}.wav"
+			input_file = "#{Rails.root}/app/assets/meows/#{note["note"]}.wav"
 			note_to_adjust = FFMPEG::Movie.new("#{Rails.root}/app/assets/meows/#{note["note"]}.wav")
-			# if note_to_adjust.duration >= note["duration"].to_f
-				note_to_adjust.transcode(output_file, ["-ss", "0", "-t", note["duration"]])
-			# else
-			# 	loops = number_of_loops(note_to_adjust.duration, note["duration"].to_f)
-			# 	temp_output_file = "#{Rails.root}/app/assets/loops/#{note["note"]}-#{Time.now}-#{index}.wav"
-			# 	note_duration = note["duration"]
-			# 	create_looped_file(loops, note_to_adjust, output_file, temp_output_file, note_duration)
-			# end
+			note_duration = note["duration"]
+			transcode_params = ["-ss", "0", "-t", note_duration]
+
+			if note_to_adjust.duration >= note["duration"].to_f
+				note_to_adjust.transcode(output_file, transcode_params)
+			else
+				loops = number_of_loops(note_to_adjust.duration, note["duration"].to_f).to_s
+				looped_file = "#{Rails.root}/app/assets/loops/#{note["note"]}-#{Time.now}-#{index}.wav"
+				create_looped_file(loops, input_file, output_file, looped_file, note_duration)
+			end
+
 			notes << output_file
 			index += 1
 		end
@@ -39,17 +43,13 @@ class MeowFileMatcher
 		(note_length/file_length).ceil
 	end
 
-	def create_looped_file(loops, note_to_adjust, output_file, temp_output_file, note_duration)
-		note_to_adjust.transcode(temp_output_file, ["-stream_loop", "#{loops}"])
-		# ffmpeg -stream_loop 2 -i ~/Code/personal/meowifier/app/assets/meows/a-3.wav
-		# ~/Code/personal/meowifier/app/assets/meows/looped.wav
-		binding.pry
-
-		crop_looped_file(temp_output_file, output_file, note_duration)
+	def create_looped_file(loops, input_file, output_file, looped_file, note_duration)
+		system "ffmpeg -stream_loop #{loops} -i #{input_file} '#{looped_file}'"
+		crop_looped_file(looped_file, output_file, note_duration)
 	end
 
-	def crop_looped_file(temp_output_file, output_file, note_duration)
-		cropped_loops = FFMPEG::Movie.new(temp_output_file)
+	def crop_looped_file(looped_file, output_file, note_duration)
+		cropped_loops = FFMPEG::Movie.new(looped_file)
 		cropped_loops.transcode(output_file, ["-ss", "0", "-t", note_duration])
 	end
 end
