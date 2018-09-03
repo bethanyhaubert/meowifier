ffb52c9be7e6081c2042ffbd3ba9722ec1cafc9b
“Beth <bethany.haubert@gmail.com>
2018-09-03 20:11:09 +0200

    added styling

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326212041) do
+ActiveRecord::Schema.define(version: 2017_03_26_212041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "songs", force: :cascade do |t|
-    t.string   "file"
-    t.string   "title"
-    t.string   "meowified_file"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string "file"
+    t.string "title"
+    t.string "meowified_file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
